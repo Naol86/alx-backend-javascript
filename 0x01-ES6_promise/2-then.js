efcc856aa3832cc9a1121a9c54c04f2ebd248976
naol86 <naol.kasinet@a2sv.org>
2024-06-20 17:32:23 +0300

    test

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error('The fake API is not working currently'))
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
