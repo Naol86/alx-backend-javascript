00e02a82a1712eb0a03575f7f9f6ecb3ad709e03
naol86 <naol.kasinet@a2sv.org>
2024-06-20 11:08:18 +0300

    test

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // unused variable
+    const task2 = false; // unused variable
   }
 
   return [task, task2];
