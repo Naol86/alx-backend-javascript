b05a53741490c668028109ad0ec3b0ea56658282
naol86 <naol.kasinet@a2sv.org>
2024-06-20 17:42:29 +0300

    test

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .them((values) =>
+    .then((values) =>
       console.log(values[0].body, values[1].firstName, values[1].lastName),
     )
     .catch(() => console.log('Signup system offline'));
