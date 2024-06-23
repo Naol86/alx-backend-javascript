e4c058314db79969031cf0249000da168d686dc3
naol86 <naol.kasinet@a2sv.org>
2024-06-23 16:18:43 +0300

    test

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (value instanceof String === false) {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value instanceof String === false) {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = value;
+  }
+}
