8f545dd72e5cb204b8c7c9ddde79e0fc1c4068a6
naol86 <naol.kasinet@a2sv.org>
2024-06-28 22:50:24 +0300

    test

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const students = [
-    { id: 1, firstName: 'John', location: 'US' },
-    { id: 2, firstName: 'Bob', location: 'Europe' },
-    { id: 3, firstName: 'Alice', location: 'US' },
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return students;
 }
